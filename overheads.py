abedce3d6de14b92f3753580530f7ba2ea6bc707
neulbin <S10255941@connect.np.edu.sg>
2023-08-07 18:03:04 +0800

    finished code for overhead, will do vetting on wed

--- overheads.py
+++ overheads.py
@@ -17,3 +17,21 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #get the type of expense and its percentage
         #and append the overheads list
         overheads.append([row[0],row[1]])  
+
+
+def overhead_function():
+    highest_overhead_category = None
+    max_expense = 0 
+
+    for category, expense in overheads:
+        # convert the percentage of the expense from a string to a float
+        expense_percentage = float(expense)
+
+        # compare current expense_percentage to max_expense
+        if expense_percentage > max_expense:
+            # if current expense_percentage is greater than max_expense
+            # update max_expense to the new value and store the category
+            max_expense = expense_percentage
+            highest_overhead_category = category
+
+        return highest_overhead_category, expense_percentage
