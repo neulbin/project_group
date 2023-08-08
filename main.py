9a2af11473ab54d07415fd1fd742aad41e034842
neulbin <S10255941@connect.np.edu.sg>
2023-08-08 16:19:10 +0800

    changed test.txt to summary_report.txt

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cash_on_hand, overheads, profit_loss
 highest_overhead_category, expense_percentage = overheads.overhead_function()
 
 # create a file path and a new file.txt
-file_path_write = Path.cwd()/'test.txt'
+file_path_write = Path.cwd()/'summary_report.txt'
 file_path_write.touch()
 
 with file_path_write.open(mode='w', encoding="'UTF-8") as file:
