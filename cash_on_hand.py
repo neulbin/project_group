cb4130577356c31185f784dc88e48d43ec8a6271
neulbin <S10255941@connect.np.edu.sg>
2023-08-09 22:39:05 +0800

    edited comments

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -19,6 +19,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         cashOnHand.append([str(row[0]), str(row[1])])
 
 #-------------------------------------------------------------#
+
 def coh_function(cashOnHand):
     previous_cash = None
     highest_surplus = {'day': None, 'amount': 0}
@@ -32,12 +33,14 @@ def coh_function(cashOnHand):
 
         # determines whether difference is a deficit or a surplus
         if previous_cash is not None:
+            # subtract the current day's cash on hand from the previous day's cash on hand
             cash_diff = cash - previous_cash
             
             # if the difference is below 0 (a negative number), it is a deficit
             if cash_diff < 0:
                 output += f"[CASH DEFICIT] Day: {day}, Amount: USD{abs(cash_diff)}\n"
-             # if the difference is above 0 (a psoitive number), it is a surplus
+
+            # if the difference is above 0 (a psoitive number), it is a surplus
             else:
                 output += f"[CASH SURPLUS] Day: {day}, Amount: USD{cash_diff}\n"
 
@@ -45,10 +48,12 @@ def coh_function(cashOnHand):
                 if cash_diff > highest_surplus['amount']:
                     highest_surplus['day'] = day
                     highest_surplus['amount'] = cash_diff
-                    
+
+        # the current day's cash on hand will become the previous day's cash on hand as the code loops
         previous_cash = cash
 
     if highest_surplus['day'] is not None:
         output += f"[HIGHEST CASH SURPLUS] Day: {highest_surplus['day']}, Amount: USD{highest_surplus['amount']}"
 
+    # return all the outputs
     return output
--- profit_loss.py
+++ profit_loss.py
@@ -19,6 +19,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         profitLoss.append([str(row[0]), str(row[4])])
 
 #-------------------------------------------------------------#
+
 def profitLoss_function(profitLoss):
     previous_net_profit = None
     highest_increment = {'day': None, 'amount': 0}
@@ -32,10 +33,14 @@ def profitLoss_function(profitLoss):
 
         # determines whether difference is a deficit or a surplus
         if previous_net_profit is not None:
+            # subtract the current day's net profit from the previous day's net profit
             net_profit_diff = net_profit - previous_net_profit
 
+            # if the difference is below 0 (a negative number), it is a deficit
             if net_profit_diff < 0:
                 output += f"[NET PROFIT DEFICIT] Day: {day}, Amount: USD{abs(net_profit_diff)}\n"
+                
+            # if the difference is above 0 (a psoitive number), it is a surplus
             else:
                 output += f"[NET PROFIT SURPLUS] Day: {day}, Amount: USD{net_profit_diff}\n"
 
@@ -44,9 +49,11 @@ def profitLoss_function(profitLoss):
                     highest_increment['day'] = day
                     highest_increment['amount'] = net_profit_diff
 
+        # the current day's net profit will become the previous day's net profit as the code loops
         previous_net_profit = net_profit
 
     if highest_increment['day'] is not None:
         output += f"[HIGHEST NET PROFIT SURPLUS] Day: {highest_increment['day']}, Amount: USD{highest_increment['amount']}"
 
+    # return all the outputs
     return output
